0264cd9ffe0e688d44185118d4ddd88d09c3450e
emp3thy@googlemail.com <emp3thy@googlemail.com>
2020-11-01 20:32:36 +0000

    cope with fluctuations of potentiometer

--- AllEffects_FastLED/effectChanging.h
+++ AllEffects_FastLED/effectChanging.h
@@ -1,21 +1,28 @@
 #include <FastLED.h>
 
-void changeEffect(byte effect,byte currentEffect)
+void changeEffect(byte effect, byte currentEffect)
 {
   if (effect != currentEffect)
   {
     gHue = 0;
-    selectedEffect=effect;
+    selectedEffect = effect;
     FastLED.setBrightness(BRIGHTNESS);
     EEPROM.write(2, effect);
     asm volatile("  jmp 0");
   }
 }
+int lastSetResistence = -20;
 
 void convertToSelectedEffect(int resistence)
 {
-  byte result = resistence / 70;
-  if  (result>14)
-    result=14;
-  changeEffect(result, selectedEffect);
+  int difference = resistence - lastSetResistence;
+  // just in case the value is fluctuating a couple of points in either direction
+  //because I bought a cheap potentiometer
+  if (lastSetResistence > 10 || lastSetResistence < -10)
+  {
+    byte result = resistence / 70;
+    if (result > 14)
+      result = 14;
+    changeEffect(result, selectedEffect);
+  }
 }
