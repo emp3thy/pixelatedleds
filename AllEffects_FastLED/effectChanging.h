3a949924140eb5bc8fc63fe75474c81a57230b9b
emp3thy@googlemail.com <emp3thy@googlemail.com>
2020-11-01 21:04:42 +0000

    commit

--- AllEffects_FastLED/effectChanging.h
+++ AllEffects_FastLED/effectChanging.h
@@ -7,7 +7,6 @@ void changeEffect(byte effect, byte currentEffect)
     gHue = 0;
     selectedEffect = effect;
     FastLED.setBrightness(BRIGHTNESS);
-    EEPROM.write(2, effect);
     asm volatile("  jmp 0");
   }
 }
@@ -18,11 +17,12 @@ void convertToSelectedEffect(int resistence)
   int difference = resistence - lastSetResistence;
   // just in case the value is fluctuating a couple of points in either direction
   //because I bought a cheap potentiometer
-  if (lastSetResistence > 10 || lastSetResistence < -10)
+  if (difference > 5 || difference < -5)
   {
     byte result = resistence / 70;
     if (result > 14)
       result = 14;
+      lastSetResistence=resistence;
     changeEffect(result, selectedEffect);
   }
 }
