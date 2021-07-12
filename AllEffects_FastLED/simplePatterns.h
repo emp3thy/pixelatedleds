25318a311231bcf5beeb7d261b2af2f8a3c2e76e
emp3thy@googlemail.com <emp3thy@googlemail.com>
2021-07-12 12:53:48 +0100

    made fill a little more interesting

--- AllEffects_FastLED/simplePatterns.h
+++ AllEffects_FastLED/simplePatterns.h
@@ -46,8 +46,8 @@ void addGlitter( fract8 chanceOfGlitter)
 
 void fill_grad() {
   
-  uint8_t starthue = beatsin8(5, 0, 255);
-  uint8_t endhue = beatsin8(7, 0, 255);
+  uint8_t starthue = beatsin8(2, 0, 255);
+  uint8_t endhue = beatsin8(15, 0, 255);
   
   if (starthue < endhue) {
     fill_gradient(leds, NUM_LEDS, CHSV(starthue,255,255), CHSV(endhue,255,255), FORWARD_HUES);    // If we don't have this, the colour fill will flip around. 
