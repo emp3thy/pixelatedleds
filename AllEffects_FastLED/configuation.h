7b0eb989b631b81beda08ed260bbebdf8cb2ed71
emp3thy@googlemail.com <emp3thy@googlemail.com>
2021-07-13 22:17:10 +0100

    tweaks to make led light code work

--- AllEffects_FastLED/configuation.h
+++ AllEffects_FastLED/configuation.h
@@ -1,6 +1,6 @@
 //for differentiating between doing a line and a matrix
 #define ISMATRIX false
-#define NUM_LEDS 100
+#define NUM_LEDS 26
 #define COL_OFFSET 5 //for when we do a 5 by 20 matrix
 //#define COL_OFFSET 9 - for when we are doing a 10 by 10 matrix
 #define NUM_ROWS 5
--- AllEffects_FastLED/effectChanging.h
+++ AllEffects_FastLED/effectChanging.h
@@ -6,7 +6,7 @@ byte convertToSelectedEffect(int resistence)
 {
   int listOfPatternsForRectangularMatrix[] = {0, 1, 2, 3, 14, 15, 6, 7, 17, 9, 16, 11, 12, 13};
   int listOfPatternsForSquareMatrix[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-  int listOfPatternsForSimpleLedStrip[] = {0, 1, 18, 3, 14, 15, 6, 7, 8, 16, 10, 17, 12, 13};
+  int listOfPatternsForSimpleLedStrip[] = {0,1,6,7,8, 10, 12, 14, 15, 16, 17, 18,0, 13};
   int difference = resistence - lastSetResistence;
   byte result = resistence / 78;
   if (result > 13)
